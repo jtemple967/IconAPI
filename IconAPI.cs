5b8c57f06a78318add4270e84bd6ee18e329b4ad
Jonas Temple <jtemple@yhti.net>
2011-12-15 22:45:14 -0600

    More changes...

--- IconAPI.cs
+++ IconAPI.cs
@@ -14,39 +14,67 @@ namespace IconAPI
 		private string phone;
 		private string userName;
 		private string password;
+		private string session;
 		
 		public IconAuth(string _phone, string _userName, string _password)
 		{
 			phone = _phone;
 			userName = _userName;
 			password = _password;
+			session = string.Empty;
 		}
 		
 		public string Phone
 		{
 			get { return phone; }
-			set { phone = value; }
+			set { 
+					phone = value; 
+					session = string.Empty;
+				}
 		}
 		
 		public string UserName
 		{
 			get { return userName; }
-			set { userName = value; }
+			set { 	
+					userName = value; 
+					session = string.Empty;
+				}
 		}
 		
 		public string Password
 		{
 			get { return password; }
-			set { password = value; }
+			set { 	
+					password = value; 
+					session = string.Empty;
+				}
+		}
+		
+		public string Session
+		{
+			get { return session; }
+			set { 
+					session = value; 
+				}
 		}
 		
 		public string GetAuthXML()
 		{
-			return "<Auth>" +
-					"<Phone>" + phone + "</Phone>" + 
+			string auth = string.Empty;
+
+			auth = "<Auth>";
+			if (session != string.Empty)
+				auth += "<Session>" + session + "</Session>";
+			else
+			{
+				auth += "<Phone>" + phone + "</Phone>" + 
 					"<Username>" + userName + "</Username>" +
-					"<Password>" + password + "</Password>" +
-					"</Auth>";
+					"<Password>" + password + "</Password>";
+			}
+			auth += "</Auth>";
+			
+			return auth;	
 		}
 	}
 	
