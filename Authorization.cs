9fe399c6af5a9a85f01a94f5925d863727c402b0
Jonas Temple <jtemple@yhti.net>
2012-09-12 21:14:34 -0500

    * Begin adding retrieval of system authorizations

--- Authorization.cs
+++ Authorization.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Net;
+using System.Xml;
 
 namespace IconAPI
 {
@@ -71,6 +76,98 @@ namespace IconAPI
 		}
 	}
 	
+	public class Permissions
+	{
+		private Collection<Permission> permissions;
+		private string role;
+		private XmlDocument xmlDoc;
+		private string httpRequest;
+		
+		public Permissions(IconAuth auth)
+		{
+			xmlDoc = new XmlDocument();
+			permissions = new Collection<Permission>();
+			
+			// Form the http request
+			httpRequest = HttpRequestHandler.BuildRequest("<Module>user</Module>" +
+														"<Section>permissions</Section>",
+														null,
+														auth);
+			// Send the request
+			HttpWebResponse response = HttpRequestHandler.SendRequest(httpRequest);
+			// Load the response into an XML document
+			xmlDoc.Load(new StreamReader(response.GetResponseStream()));
+			// Extract the security token from the response
+			HttpRequestHandler.GetSession(xmlDoc, ref auth);
+			// Load the permissions
+			LoadPermissions();
+			// Get the role
+			XmlNode wrkNode = xmlDoc.SelectSingleNode("role");
+			role = wrkNode.Value;
+		}
+		
+		public Collection<Permission> Entries
+		{
+			get { return permissions; }
+		}
+		
+		public string Role
+		{
+			get { return role; }
+		}
+		
+		private void LoadPermissions()
+		{
+			// Get the permission nodes
+			XmlNodeList nodes = xmlDoc.SelectNodes("/iconresponse/Permissions");
+			foreach (XmlNode node in nodes)
+			{
+				// Create a permission entry and add to the collection
+				Permission entry = new Permission();
+				entry.Module = node.Name;
+				XmlNodeList sectionNodes = node.ChildNodes;
+				foreach (XmlNode sectionNode in sectionNodes)
+				{
+					entry.Section = sectionNode.Name;
+					XmlNode wrkNode = sectionNode.SelectSingleNode("create");
+					bool wrkBool = false;
+					bool.TryParse(wrkNode.Value, out wrkBool);
+					entry.Create = wrkBool;
+					wrkNode = sectionNode.SelectSingleNode("read");
+					bool.TryParse(wrkNode.Value, out wrkBool);
+					entry.Read = wrkBool;
+					wrkNode = sectionNode.SelectSingleNode("update");
+					bool.TryParse(wrkNode.Value, out wrkBool);
+					entry.Update = wrkBool;
+					wrkNode = sectionNode.SelectSingleNode("delete");
+					bool.TryParse(wrkNode.Value, out wrkBool);
+					entry.Delete = wrkBool;
+				}
+			}
+		}
+	}
+					
+	
+	public class Permission
+	{
+		public string Module { get; set; }
+		public string Section { get; set; }
+		public bool Create { get; set; }
+		public bool Read { get; set; }
+		public bool Update { get; set; }
+		public bool Delete { get; set; }
+		
+		public Permission(string _module = "", string _section = "", bool _create = false, 
+							bool _read = false, bool _update = false, bool _delete = false)
+		{
+			Module = _module;
+			Section = _section;
+			Create = _create;
+			Read = _read; 
+			Update = _update;
+			Delete = _delete;
+		}
+	}
 	
 }
 
